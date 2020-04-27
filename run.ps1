a048d800573669c996ebffd52c885b12633ce283
nowakm20 <maciej.nowak.mn1@contractors.roche.com>
2020-04-27 18:18:12 +0200

    added name to container

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 docker build -t lawnmawer:latest -f "Dockerfile.dev" .
 docker run `
-  -it --rm `
+  -it --rm --name lawnmawer `
   -v "$(pwd)/src:/app/src" `
   -v "$(pwd)/public:/app/public" `
   -p 3000:3000 lawnmawer:latest
