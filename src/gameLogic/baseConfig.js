40dbdea8e7498cc6a22077a7ccfe5847c1a4248d
Szymon Nowak <sznowak13@gmail.com>
2020-05-01 20:37:13 +0200

    Added upgrade class

--- src/gameLogic/baseConfig.js
+++ src/gameLogic/baseConfig.js
@@ -1,5 +1,30 @@
 import { buildRange } from './utils';
 
-export const tickRates = buildRange(1000, x => x*0.9, x => x > 5);
-export const fieldSizes = [10, 20, 40, 80, 100, 200, 400];
-export const growthRates = buildRange(4, x => x+2, x => x < 40);
+
+
+class Upgrade {
+    constructor(name, basePrice, generator, priceMultiplier) {
+        this.name = name;
+        this.basePrice = basePrice;
+        this.generator = generator;
+        this.priceMultiplier = priceMultiplier;
+        this.currentValue = this.generator.next().value
+        this.currentPrice = basePrice;
+    }
+
+    nextValue = () => {
+        let nextValue = this.generator.next().value;
+        if (!nextValue) {
+            this.currentPrice = Infinity;
+        } else {
+            this.currentValue = nextValue;
+            this.currentPrice = Math.ceil(this.currentPrice * this.priceMultiplier);
+        }
+        return this.currentValue;
+    }
+}
+
+
+export const tickRate = new Upgrade('tickRate', 8, buildRange(1000, x => x * 0.9, x => x > 5), 1.2)
+export const fieldSize = new Upgrade('fieldSize', 5, buildRange(10, x => x + 10, x => x <= 400), 1.2);
+export const growthRate = new Upgrade('growthRate', 10, buildRange(4, x => x + 2, x => x < 40), 1.2);
--- src/gameLogic/config.js
+++ src/gameLogic/config.js
@@ -1,19 +1,17 @@
-import { tickRates, fieldSizes, growthRates } from './baseConfig';
+import { tickRate, fieldSize, growthRate } from './baseConfig';
 
 export default class Config {
     constructor(baseColor, multiplier, basePrices) {
         this.baseColor = baseColor;
-        this.sizes = fieldSizes;
-        this.tickRates = tickRates;
-        this.growthRates = growthRates;
+        this.size = fieldSize;
+        this.tickRate = tickRate;
+        this.growthRate = growthRate;
         this.multiplier = multiplier;
         this.basePrices = basePrices;
     }
 
-    getNext(what, current) {
-        const possibilities = this[what + "s"];
-        const i = possibilities.indexOf(current);
-        if (i >= possibilities.length) return null;
-        return possibilities[i+1];
+    getNext(what) {
+        return this[what].nextValue()
     }
 };
+
