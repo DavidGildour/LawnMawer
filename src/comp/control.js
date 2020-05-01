e29f0013c9e52dcaf477906616be635c1598eb37
Szymon Nowak <sznowak13@gmail.com>
2020-05-01 20:38:29 +0200

    Moved formatting to view component

--- src/comp/control.js
+++ src/comp/control.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import '../css/control.css';
 
+
+function formatPrice(price) {
+    if (price === Infinity) {
+        return 'MAX'
+    } else {
+        return `${price} $`
+    }
+}
+
 export default function Control(props) {
+    const ticksPerSecond = (1000 / props.curTickRate).toPrecision(4).replace(/\.?0+$/, "");
+    const currentTickRate = `${ticksPerSecond} ticks per second`,
+      currentSize = `${props.curSize} x ${props.curSize} tiles`,
+      currentGrowthRate = `Growing ${props.curGrowthRate} tiles per tick`;
     return (
         <div className="green rounded upgrades">
             <div>{props.cash} $</div>
-            <button onClick={props.upgradeTick}>Upgrade Tick Rate ({props.tickRatePrice} $)</button> {props.curTickRate}<br />
-            <button onClick={props.upgradeSize}>Upgrade Field Size ({props.sizePrice} $)</button> {props.curSize}<br />
-            <button onClick={props.upgradeGrowth}>Upgrade Growth Rate ({props.growthRatePrice} $)</button> {props.curGrowthRate}<br />
+            <button onClick={props.upgradeTick}>Upgrade Tick Rate ({formatPrice(props.tickRatePrice)})</button> {currentTickRate}<br />
+            <button onClick={props.upgradeSize}>Upgrade Field Size ({formatPrice(props.sizePrice)})</button> {currentSize}<br />
+            <button onClick={props.upgradeGrowth}>Upgrade Growth Rate ({formatPrice(props.growthRatePrice)})</button> {currentGrowthRate}<br />
             <button onClick={props.godMode}>Add cash</button>
         </div>
     )
