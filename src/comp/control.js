a2e7633b8adc9a3ffeabacfd4e02d6595ae147b3
DavidGildour <davidgildour@gmail.com>
2020-05-03 20:02:57 +0200

    added prettier display of upgrade buttons and prepared Control component for recieving null values for prices

--- src/comp/control.js
+++ src/comp/control.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import '../css/control.css';
 
+function formatPrice(price) {
+    return price === null ? "MAX" : price.toString();
+}
+
+function UpgradeButton(props) {
+    return (
+        <div className="upgrade">
+            <button className="upgradeButton" onClick={props.upgradeFunc}>
+                {props.text} ({formatPrice(props.currentPrice)})
+            </button>
+            <span className="upgradeText">&nbsp;{props.currentValue}</span>
+        </div>
+    )
+}
+
 export default function Control(props) {
     return (
         <div className="green rounded upgrades">
             <div>{props.cash.toString()}</div>
-            <button onClick={props.upgradeTick}>Upgrade Tick Rate ({props.tickRatePrice.toString()})</button> {props.curTickRate}<br />
-            <button onClick={props.upgradeSize}>Upgrade Field Size ({props.sizePrice.toString()})</button> {props.curSize}<br />
-            <button onClick={props.upgradeGrowth}>Upgrade Growth Rate ({props.growthRatePrice.toString()})</button> {props.curGrowthRate}<br />
-            <button onClick={props.godMode}>Add cash</button>
+            <UpgradeButton upgradeFunc={props.upgradeTick} text="Upgrade Tick Rate" currentPrice={props.tickRatePrice} currentValue={props.curTickRate} />
+            <UpgradeButton upgradeFunc={props.upgradeSize} text="Upgrade Field Size" currentPrice={props.sizePrice} currentValue={props.curSize} />
+            <UpgradeButton upgradeFunc={props.upgradeGrowth} text="Upgrade Growth Rate" currentPrice={props.growthRatePrice} currentValue={props.curGrowthRate} />
+            <UpgradeButton upgradeFunc={props.godMode} text="Add $$$" currentPrice={0} currentValue={100_000} />
         </div>
     )
 };
