49f6925171e4ca12edcc689a1d4c2921e4aacdec
Basia <bachna22@gmail.com>
2020-05-07 20:49:19 +0200

    Animacja kosiary

--- src/comp/header.js
+++ src/comp/header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../css/header.css';
+import lawnmawer from '../static/lawnmawer.gif'
 
 
 function Header() {
     return (
         <header className="green rounded">
-            <h1 class="title animated slideInLeft delay-1s">- LawnMawer -</h1>
+            <h1 class="title animated slideInLeft">- LawnMawer -<img src={lawnmawer} alt="kosiara" height="80" width="80" /></h1>
+            
             
         </header>
         
